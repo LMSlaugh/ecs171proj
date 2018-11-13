41b3d5c54792139ce87d22b962042db50c5a9ac7
Calvin Cramer <cjcramer@ucdavis.edu>
2018-11-12 19:03:17 -0800

    Some work

--- ann/ann.py
+++ ann/ann.py
@@ -1,5 +1,7 @@
 from keras.models import Sequential
 from keras.layers import Dense
+import pandas
+import data_parse
 
 def BuildAnn(xTrain,yTrain,hiddenLayerSize):
     model = Sequential()
@@ -9,4 +11,17 @@ def BuildAnn(xTrain,yTrain,hiddenLayerSize):
     model.compile(loss='binary_crossentropy',optimizer='sgd',metrics=['accuracy'])
     model.fit(xTrain,yTrain,epochs=20,batch_size=32)
     score = model.evaluate(xTrain,yTrain,batch_size=32)
-    
+
+
+data_csv = data_parse.read_data()
+print("Data csv type: " + str(type(data_csv)))
+data = data_csv.values
+print("Data type: " + str(type(data)))
+print(data)
+
+#activation functions:
+#step
+#sigmoid
+#ReLu
+#tanh
+activation_functions = ['tanh', 'sigmoid', 'elu', 'selu', 'softplus']
--- ann/data_parse.py
+++ ann/data_parse.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
-data_csv = pd.read_csv('scc_data_to_use.csv', index_col = 0, parse_dates=True, infer_datetime_format=True)
-print(data_csv.shape[1])
+def read_data():
+    data_csv = pd.read_csv('scc_data_to_use.csv', index_col = 0, parse_dates=True, infer_datetime_format=True)
+    return data_csv
