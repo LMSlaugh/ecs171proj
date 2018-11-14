1a0ca5239d5e8342fc61321e8fc548c6adaa1277
Calvin Cramer <cjcramer@ucdavis.edu>
2018-11-13 17:29:03 -0800

    Modified ann.py

--- ann/ann.py
+++ ann/ann.py
@@ -19,9 +19,13 @@ data = data_csv.values
 print("Data type: " + str(type(data)))
 print(data)
 
+#all permutations of these for grid search:
 #activation functions:
-#step
-#sigmoid
-#ReLu
-#tanh
-activation_functions = ['tanh', 'sigmoid', 'elu', 'selu', 'softplus']
+activation_functions = ['tanh', 'sigmoid', 'elu', 'selu', 'softplus', 'softsign', 'relu', 'hard_sigmoid']
+number_of_nodes_per_layer = range(1,9)  # 1 to 8 inclusive
+number_of_hidden_layers   = range(1,5)  # 1 to 4 inclusive
+batch_sizes = [1,2,4,8,16,32,64]
+learning_rates = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1]
+
+print("\nNumber of permutations for grid search: ", end="")
+print(len(activation_functions) * len(number_of_nodes_per_layer) * len(number_of_hidden_layers) * len(batch_sizes) * len(learning_rates))
