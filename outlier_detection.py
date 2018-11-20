12b9566fed5d8966dc4c459a2fad4646d6ced139
Victoria Salova <victoria.salova@gmail.com>
2018-11-19 21:57:14 -0800

    added outlier detection script and data with outliers removed

--- outlier_detection.py
+++ outlier_detection.py
@@ -2,23 +2,20 @@ import pandas as pd
 import numpy as np
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.ensemble import IsolationForest
-import matplotlib.pyplot as plt
 
 # Importing data
-data = pd.read_csv("Dataset/scc_data_to_use.csv", header=0)
+data = pd.read_csv("Dataset/scc_data_to_use_normalized_features_removed.csv", header=0)
 data = data.drop(data.columns[0], axis=1) # Remove time of day
 
 column_names = data.columns.values[:-1]
 featureData = data.iloc[:, :-1].values # Remove output labels
 outputLabels = data.iloc[:,-1].values # Get only output labels
 
-data = data.values
-
 # Isolation forest
 clf = IsolationForest(contamination='auto', behaviour='new')
 
-clf.fit(data)
-detected_results_IS = clf.predict(data)
+clf.fit(featureData)
+detected_results_IS = clf.predict(featureData)
 
 outliers_IS = []
 for i in range(len(detected_results_IS)):
@@ -28,7 +25,7 @@ for i in range(len(detected_results_IS)):
 # LOF
 clf = LocalOutlierFactor(n_neighbors=5, contamination='auto')
 
-detected_results_LOF = clf.fit_predict(data)
+detected_results_LOF = clf.fit_predict(featureData)
 
 outliers_LOF = []
 for i in range(len(detected_results_LOF)):
@@ -36,8 +33,8 @@ for i in range(len(detected_results_LOF)):
         outliers_LOF.append(i)
 
 # use LOF results to remove the outlieres
-data = np.delete(data, (outliers_LOF), axis=0)
+featureData = np.delete(featureData, (outliers_LOF), axis=0)
 
-data = pd.DataFrame(data)
+featureData = pd.DataFrame(featureData)
 
-#output = data.to_csv('Dataset/scc_data_to_use_no_outliers.csv', index=False, header=None)
+output = data.to_csv('Dataset/scc_data_to_use_no_outliers.csv', index=False, header=None)
