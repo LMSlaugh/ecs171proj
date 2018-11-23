de3d4405096666a1e6f0b0faca82f2663f1c4609
Lisa Slaughter <lisa.m.slaughter@gmail.com>
2018-11-23 13:56:44 -0800

    Ran complete data set through logistic regression

--- Logistic Regression/LinearRegression.py
+++ Logistic Regression/LinearRegression.py
@@ -1,41 +1,35 @@
 import pandas as pd
 import numpy as np
+import PI_client as pc
 import matplotlib
 import sklearn as sk
 from sklearn import linear_model
-import os
 
-# Reading in data, setting index
-os.chdir("C:\\Users\\lisam\\Desktop\\Repositories\\ecs171proj")
-df = pd.read_csv('Dataset/scc_data_to_use.csv')
-df.index = df.iloc[:,0]
-df = df.drop(["Unnamed: 0"], axis=1)
-
+# Reading in data
+df = pd.read_csv('data/scc_data_to_use_no_outliers.csv')
 
 # Splitting into training and testing sets
 train = df.sample(frac=.7).copy()
 test = df.drop(train.index).copy()
-
 
 # Separating features from labels
 col = df.columns
 col = col[0:len(col)-1]
 
 x_train = pd.DataFrame(index=train.index, data=train[col])
-y_train= pd.DataFrame(index=train.index, data=train["Occupied"])
+y_train= pd.DataFrame(index=train.index, data=train["Occupancy"])
 
 x_test = pd.DataFrame(index=test.index, data=test[col])
-y_test= pd.DataFrame(index=test.index, data=test["Occupied"])
-
+y_test= pd.DataFrame(index=test.index, data=test["Occupancy"])
 
 # Creating and training logistic regression model
 mod = linear_model.LogisticRegression(solver="sag",max_iter=3000)
-LR_model = mod.fit(x_train,y_train["Occupied"])
-
+LR_model = mod.fit(x_train,y_train["Occupancy"])
 
 # Extracting accuracy
 accuracy = LR_model.score(x_test,y_test)
-
+accuracy
 
 # Converting accuracy to error
 error = 1-accuracy
+error
